3d18c4bd08bd4bfbb76fe79e91dc04722769da40
Thomas Sjolshagen <thomas@eighty20results.com>
2017-09-11 11:28:34 -0600

    ENHANCEMENT: Display license settings on Membership Level configuration page for Paid Memberships Pro ENHANCEMENT: Use filters to include payment service specific settings/info when generating license ENHANCEMENT: Don't expect the Software License Manager software to be installed locally ENHANCEMENT: Let admin specify the number of domains a license can be applied to in the PMPro Level settings BUG FIX: Append the new membership level(s) to the list of levels to check out (MMPU/Single level handling for Paid Memberships Pro) BUG FIX: Load correct path to the License email for Paid Memberships Pro BUG FIX: Argument order for getBillingInfo() filter handler (lost user info)

--- class/class.pmpro.php
+++ class/class.pmpro.php
@@ -50,13 +50,13 @@ class PMPro {
 		
 		add_action( 'pmpro_save_membership_level', array( $this, 'saveSettings' ), 10, 1 );
 		add_action( 'pmpro_after_change_membership_level', array( $this, 'levelChanged' ), 10, 3 );
-		add_action( 'pmpro_membership_level_after_other_settings', array( $this, 'licenseSettings', ), 10, 0 );
+		add_action( 'pmpro_membership_level_after_other_settings', array( $this, 'licenseSettings', ), 10, 1 );
 		
 		add_filter( 'e20r-license-server-txn-id', array( $this, 'getTransactionId' ), 10, 3 );
 		add_filter( 'e20r-license-server-billing-info', array( $this, 'getBillingInfo' ), 10, 3 );
-		add_filter( 'e20r-licensing-server-expiration-date', array( $this, 'expires' ), 10, 2 );
+		add_filter( 'e20r-licensing-server-expiration-date', array( $this, 'expires' ), 10, 3 );
 		add_filter( 'e20r-licensing-server-client-key', array( $this, 'createLicenseKey' ), 10, 4 );
-		add_filter( 'e20r-license-server-domains-per-license', array( $this, 'allowedDomains' ), 10, 3 );
+		add_filter( 'e20r-license-server-domains-per-license', array( $this, 'domains' ), 10, 3 );
 		add_filter( 'e20r-license-server-license-name', array( $this, 'licenseName' ), 10, 3 );
 	}
 	
@@ -83,12 +83,17 @@ class PMPro {
 			
 			// Handle situations where the level is assigned to the user
 			if ( empty( $pmpro_checkout_levels ) ) {
-				$pmpro_checkout_levels = array( pmpro_getLevel( $level_id ) );
+				
+				if ( ! is_array( $pmpro_checkout_levels ) ) {
+					$pmpro_checkout_levels = array();
+				}
 			}
 			
-			$utils->log( "Found an order? " . ( isset( $order->membership_id ) ? "Yes" : "No" ) );
+			$pmpro_checkout_levels[] = pmpro_getLevel( $level_id );
 			
-			$this->checkout( $user_id, $order );
+			$utils->log( "Found a preexisting order? " . ( isset( $order->membership_id ) ? "Yes" : "No" ) );
+			
+			$this->checkout( $user_id, $order, $level_id );
 		}
 		
 		// TODO: Handle membership cancellations
@@ -106,24 +111,14 @@ class PMPro {
 	 */
 	public function licenseSettings( $echo = true ) {
 		
-		$controller = Controller::getInstance();
-		$utils      = Utilities::get_instance();
-		
-		if ( ! $controller->checkPrereqs() ) {
-			return;
-		}
+		$utils = Utilities::get_instance();
 		
 		$level_id  = $utils->get_variable( 'edit', - 1 );
-		$value_map = get_option( 'e20r_pmpro_license_server' );
+		$value_map = get_option( 'e20r_pmpro_license_server', array() );
 		
-		$level = pmpro_getLevel( $level_id );
-		
-		if ( pmpro_isLevelFree( $level ) ) {
-			return;
-		}
 		?>
         <div class="e20r-license-settings">
-            <h3 class="e20r-license-settings-header topborder"><?php echo __( "License Settings", "e20r-add-license-on-purchase" ); ?></h3>
+            <h3 class="e20r-license-settings-header topborder"><?php _e( "License Settings", "e20r-add-license-on-purchase" ); ?></h3>
             <table class="form-table">
                 <tbody>
                 <tr>
@@ -132,7 +127,7 @@ class PMPro {
                     </th>
                     <td>
                         <input id="e20r-license-key" name="e20r-license-key" type="text"
-                               value="<?php echo( ! empty( $value_map[ $level_id ]['key'] ) ? $value_map[ $level_id ]['key'] : null ); ?>"/>
+                               value="<?php echo( ! empty( $value_map[ $level_id ]['license_key'] ) ? $value_map[ $level_id ]['license_key'] : null ); ?>"/>
                     </td>
                 </tr>
                 <tr>
@@ -142,7 +137,17 @@ class PMPro {
                     <td>
                         <input id="e20r-license-name" name="e20r-license-name" type="text"
                                style="min-width: 250px; max-width: 400px; width: 100%;"
-                               value="<?php echo( ! empty( $value_map[ $level_id ]['name'] ) ? $value_map[ $level_id ]['name'] : null ); ?>"/>
+                               value="<?php echo( ! empty( $value_map[ $level_id ]['license_name'] ) ? $value_map[ $level_id ]['license_name'] : null ); ?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row" valign="top" class="e20r-license">
+                        <label for="e20r-license-sites"><?php _e( 'Number of sites included', 'e20r-add-license-on-purchase' ); ?></label>:
+                    </th>
+                    <td>
+                        <input id="e20r-license-sites" name="e20r-license-sites" type="number"
+                               style="min-width: 250px; max-width: 400px; width: 100%;"
+                               value="<?php echo( ! empty( $value_map[ $level_id ]['site_count'] ) ? $value_map[ $level_id ]['site_count'] : null ); ?>"/>
                     </td>
                 </tr>
 
@@ -159,25 +164,20 @@ class PMPro {
 	 */
 	public function saveSettings( $level_id ) {
 		
-		$controller = Controller::getInstance();
-		
-		if ( ! $controller->checkPrereqs() ) {
-			return false;
-		}
-		
 		$utils = Utilities::get_instance();
 		
 		$license_key  = $utils->get_variable( 'e20r-license-key', null );
 		$license_name = $utils->get_variable( 'e20r-license-name', null );
+		$sites        = $utils->get_variable( 'e20r-license-sites', 1 );
 		
-		if ( is_null( $license_key ) || is_null( $level_id ) || is_null( $license_name ) ) {
+		if ( is_null( $license_key ) || is_null( $license_name ) ) {
 			
 			$utils->add_message( __( 'License Name or Key missing in level definition', "e20r-add-license-on-purchase" ), 'error', 'backend' );
 			
 			return false;
 		}
 		
-		$key_map = get_option( 'e20r_pmpro_license_server' );
+		$key_map = get_option( 'e20r_pmpro_license_server', array() );
 		
 		if ( ! is_array( $key_map ) ) {
 			$key_map = array();
@@ -185,8 +185,9 @@ class PMPro {
 		
 		
 		$key_map[ $level_id ] = array(
-			'key'  => $license_key,
-			'name' => $license_name,
+			'license_key'  => $license_key,
+			'license_name' => $license_name,
+			'site_count'   => $sites,
 		);
 		
 		update_option( 'e20r_pmpro_license_server', $key_map, false );
@@ -206,17 +207,18 @@ class PMPro {
 		global $pmpro_pages;
 		global $pmpro_checkout_levels;
 		global $e20rlm_level;
+		global $e20rlm_order;
 		
-		$utils      = Utilities::get_instance();
-		$controller = Controller::getInstance();
-		$controller->checkPrereqs();
+		$e20rlm_order = $order;
+		$utils        = Utilities::get_instance();
+		$controller   = Controller::getInstance();
 		
 		$licenses = get_user_meta( $user_id, "e20r_license_user_settings", true );
 		
 		if ( empty( $licenses ) ) {
 			$licenses = array();
 		}
-  
+		
 		// Process each of the levels supplied.
 		foreach ( $pmpro_checkout_levels as $level ) {
 			
@@ -226,7 +228,7 @@ class PMPro {
 			$license_keys = get_option( 'e20r_pmpro_license_server' );
 			
 			// Skip this if the level doesn't have a license requirement
-			if ( empty( $license_keys[ $level->id ]['key'] ) ) {
+			if ( empty( $license_keys[ $level->id ]['license_key'] ) ) {
 				
 				$utils->log( "Skipping {$level->id} since it doesn't have a license to activate/create" );
 				continue;
@@ -234,7 +236,7 @@ class PMPro {
 			
 			// Load membership order & user info for the checkout
 			$user_settings = $controller->addLicense( $level->id, $user_id, 'pmpro' );
-			$licenses[] = $user_settings;
+			$licenses[]    = $user_settings;
 			
 			if ( false !== $user_settings ) {
 				
@@ -243,9 +245,9 @@ class PMPro {
 				$email->from     = pmpro_getOption( "from_email" );
 				$email->fromname = pmpro_getOption( "from_name" );
 				
-				if ( file_exists( plugin_dir_path( __FILE__ ) . "/email/e20r_license.html" ) ) {
+				if ( file_exists( plugin_dir_path( __FILE__ ) . "../email/e20r_license.html" ) ) {
 					
-					$email->body = file_get_contents( plugin_dir_path( __FILE__ ) . "/email/e20r_license.html" );
+					$email->body = file_get_contents( E20R_ALOP_DIR . "/email/e20r_license.html" );
 				} else {
 					
 					$utils->log( "License: Couldn't find the license email template" );
@@ -309,7 +311,7 @@ class PMPro {
 		$license_keys = get_option( 'e20r_pmpro_license_server' );
 		
 		if ( ! empty( $license_keys[ $id ] ) ) {
-			$key = "{$license_keys[$id]['key']}_{$random}";
+			$key = "{$license_keys[$id]['license_key']}_{$random}";
 		}
 		
 		return $key;
@@ -324,9 +326,19 @@ class PMPro {
 	 *
 	 * @return null
 	 */
-	public function getTransactionId( $txn_id, $source, $order ) {
+	public function getTransactionId( $txn_id, $source, $order = null ) {
+		
+		if ( 'pmpro' != $source ) {
+			return null;
+		}
+		
+		if ( is_null( $order ) ) {
+			global $e20rlm_order;
+			$order = $e20rlm_order;
+		}
 		
 		if ( $source == 'pmpro' ) {
+			
 			$txn_id = ! empty( $order->payment_transaction_id ) ? $order->payment_transaction_id : null;
 		}
 		
@@ -337,19 +349,20 @@ class PMPro {
 	 * Return the billing info for the license (is purchase plugin dependent)
 	 *
 	 * @param array    $info
-	 * @param string   $source
 	 * @param \WP_User $user
+	 * @param string   $source
 	 *
 	 * @return array
 	 */
-	public function getBillingInfo( $info, $source, $user ) {
+	public function getBillingInfo( $info, $user, $source ) {
 		
 		if ( 'pmpro' !== $source ) {
 			return $info;
 		}
 		
 		$user_meta = get_user_meta( $user->ID );
-		$info      = array(
+		
+		$info = array(
 			'first_name' => isset( $user_meta['pmpro_bfirstname'][0] ) ? $user_meta['pmpro_bfirstname'][0] : $user->first_name,
 			'last_name'  => isset( $user_meta['pmpro_blastname'][0] ) ? $user_meta['pmpro_blastname'][0] : $user->last_name,
 			'email'      => isset( $user_meta['pmpro_bemail'][0] ) ? $user_meta['pmpro_bemail'][0] : $user->user_email,
@@ -367,10 +380,10 @@ class PMPro {
 	 *
 	 * @return string
 	 */
-	public function licenseExpires( $expiration_date, $level_id, $source ) {
+	public function expires( $expiration_date, $level_id, $source ) {
 		
-	    $utils = Utilities::get_instance();
-	    
+		$utils = Utilities::get_instance();
+		
 		if ( ! function_exists( 'pmpro_getMembershipLevelForUser' ) || ! is_user_logged_in() || 'pmpro' != $source ) {
 			return $expiration_date;
 		}
@@ -379,10 +392,11 @@ class PMPro {
 		$level = pmpro_getMembershipLevelForUser( $current_user->ID );
 		
 		if ( $level->id != $level_id ) {
-		    $utils->log("Unable to calculate the expiration date for this license!");
-		    return $expiration_date;
-        }
-        
+			$utils->log( "Unable to calculate the expiration date for this license!" );
+			
+			return $expiration_date;
+		}
+		
 		if ( isset( $level->cycle_number ) && 1 <= $level->cycle_number ) {
 			$expiration_date = date_i18n( 'Y-m-d', strtotime( "+{$level->cycle_number} {$level->cycle_period}", current_time( 'timestamp' ) ) );
 		}
@@ -409,8 +423,36 @@ class PMPro {
 			return $name;
 		}
 		
+		$license_settings = get_option( 'e20r_pmpro_license_server' );
+		
 		$level = pmpro_getLevel( $id );
 		
-		return $level->name;
+		return ( ! empty( $license_settings[ $id ]['license_name'] ) ? $license_settings[ $id ]['license_name'] : $level->name );
 	}
+	
+	/**
+	 * Fetch info on the number of allowed domains per license from settings (PMPro)
+	 *
+	 * @param int    $count
+	 * @param int    $level_id
+	 * @param string $source
+	 *
+	 * @return bool|int
+	 */
+	public function domains( $count, $level_id, $source ) {
+		
+		if ( 'pmpro' != $source ) {
+			return $count;
+		}
+		
+		$license_settings = get_option( 'e20r_pmpro_license_server', array() );
+		
+		if ( empty( $license_settings[ $level_id ]['site_count'] ) ) {
+			return false;
+		}
+		
+		// For WooCommerce, the $count variable contains the quantity of the license ordered
+		return ( $count * absint( $license_settings[ $level_id ]['site_count'] ) );
+	}
+	
 }
